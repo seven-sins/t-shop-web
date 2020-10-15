142971e81c12748f29653283b1c714b408816162
rex.tan <rex.tan@infox.com.cn>
2020-10-15 18:00:11 +0800

    fix

---
+++ src/components/user/Edit.vue
@@ -0,0 +1,128 @@
+<template>
+    <a-form
+        :model="user" 
+        :label-col="conf.labelCol" 
+        :wrapper-col="conf.wrapperCol"
+        :form="form1"
+        >
+        <a-row type="flex" :gutter="24">
+            <a-col :span="12">
+                <a-form-item label="用户编码:">
+                    <a-input v-decorator="['user.userCode', rules.userCode]"  />
+                </a-form-item>
+            </a-col>
+            <a-col :span="12">
+                <a-form-item label="用户名:">
+                    <a-input v-model="user.userName" />
+                </a-form-item>
+            </a-col>
+        </a-row>
+        
+        <a-row type="flex" :gutter="24">
+            <a-col :span="12">
+                <a-form-item label="登录账号:">
+                    <a-input v-model="user.loginAccount" />
+                </a-form-item>
+            </a-col>
+            <a-col :span="12">
+                <a-form-item label="手机号">
+                    <a-input v-model="user.phone" />
+                </a-form-item>
+            </a-col>
+        </a-row>
+
+        <a-row type="flex" :gutter="24">
+            <a-col :span="12">
+                <a-form-item label="身份证号">
+                    <a-input v-model="user.phone" />
+                </a-form-item>
+            </a-col>
+            <a-col :span="12">
+                <a-form-item label="性别">
+                    <a-radio-group default-value="1" v-model="user.sex">
+                        <a-radio-button value="1">
+                            男
+                        </a-radio-button>
+                        <a-radio-button value="0">
+                            女
+                        </a-radio-button>
+                    </a-radio-group>
+                </a-form-item>
+            </a-col>
+        </a-row>
+        
+        <a-row type="flex" :gutter="24">
+            <a-col :span="12">
+                <a-form-item label="生日">
+                    <a-date-picker :format="conf.dateFormat" placeholder="请选择" />
+                </a-form-item>
+            </a-col>
+        </a-row>
+
+        
+        
+        <a-button type="primary" @click="handleSubmit">
+            Primary
+        </a-button>
+        
+    </a-form>
+ 
+
+
+  
+      
+ 
+</template>
+
+<script>
+
+export default {
+  name: "UserEdit",
+  components: {
+    
+  }, 
+  data(){
+    return {
+        conf: {
+            labelCol: { span: 6 },
+            wrapperCol: { span: 12 },
+            dateFormat: 'YYYY-MM-DD'
+        },
+        user: {sex: "0"},
+        form1: this.$form.createForm(this),
+        rules: {
+            userCode: { rules: [{required: true, message: "用户编码不能为空"}]}
+        }
+    }
+  },
+  props: {
+
+  },
+  created(){
+
+  },
+  mounted(){
+
+  },
+  methods: {
+    edit(record){
+        console.log(record);
+    },
+    handleSubmit(){
+        this.$emit("closeEdit", 123);
+        //
+        this.form1.validateFields((err, values) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+
+
+  
+
+</style>
--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,107 +1,120 @@
 <template>
-  <a-table :columns="columns" :dataSource="data" :pagination="page" @change="changePage" bordered>
-    <template slot="operation" slot-scope="text, record, index">
-        <span class="row-operation-btn" @click="() => edit(record)">编辑</span>
-        <span class="row-operation-btn" @click="() => del(record)">删除</span>
-    </template>
-  </a-table>
+    <div class="index-container">
+    <a-table 
+        :columns="columns" 
+        :dataSource="dataSource" 
+        :pagination="page" 
+        @change="changePage" bordered>
+
+        <template slot="operation" slot-scope="text, record, index">
+            <span class="row-operation-btn" @click="() => edit(record)">编辑</span>
+            <span class="row-operation-btn" @click="() => del(record)">删除</span>
+        </template>
+    </a-table>
+    
+    <a-modal :title="modalConf.title" v-model="modalConf.visible" :width="800" :height="800" cancelText="取消" okText="确定"
+             :maskClosable="false" destroyOnClose @ok="handleOk" style="top:20px;" :footer="null">
+      <Edit ref="userEdit" @closeEdit="closeEdit"/>
+    </a-modal>
+
+  </div>
 </template>
 
 <script>
 import http from "@/utils/http";
-import { onMounted, onUnmounted, reactive } from "@vue/composition-api";
+import Edit from "./Edit";
+
 export default {
-  name: "Home",
-  components: {
-    
-  }, 
-  setup(props, context){
-    const columns = reactive([
-        {
-            title: 'name',
-            dataIndex: 'name',
-            width: '25%',
-            scopedSlots: { customRender: 'name' },
+    name: "Home",
+    components: {
+        Edit
+    }, 
+    data(){
+      return {
+        modalConf: {
+            visible: false,
+            title: "编辑"
         },
-        {
-            title: 'age',
-            dataIndex: 'age',
-            width: '15%',
-            scopedSlots: { customRender: 'age' },
+        columns: [
+            {
+                title: 'name',
+                dataIndex: 'name',
+                width: '25%',
+                scopedSlots: { customRender: 'name' },
+            },
+            {
+                title: 'age',
+                dataIndex: 'age',
+                width: '15%',
+                scopedSlots: { customRender: 'age' },
+            },
+            {
+                title: 'address',
+                dataIndex: 'address',
+                width: '40%',
+                scopedSlots: { customRender: 'address' },
+            },
+            {
+                title: 'operation',
+                dataIndex: 'operation',
+                scopedSlots: { customRender: 'operation' },
+            },
+        ],
+        dataSource: [],
+        page: {
+            total: 0,
+            pageSize: 20,
+            current: 1,
+            size: 'big',
+            showSizeChanger: false,
+            showQuickJumper: false,
+            pageSizeOptions: ['5', '10', '20'],
+            showTotal: (tot, i) => {
+                let pageSize = this.page.pageSize
+                let nowPage = tot
+                return `共 ${nowPage} 条记录 第${this.page.current}/${Math.ceil(tot / pageSize)}页`
+            }
         },
-        {
-            title: 'address',
-            dataIndex: 'address',
-            width: '40%',
-            scopedSlots: { customRender: 'address' },
-        },
-        {
-            title: 'operation',
-            dataIndex: 'operation',
-            scopedSlots: { customRender: 'operation' },
-        },
-    ]);
+      }
+    },
+    props: {
 
-    //分页数据
-    const page = reactive({
-        total: 0,
-        pageSize: 20,
-        current: 1,
-        size: 'big',
-        showSizeChanger: false,
-        showQuickJumper: false,
-        pageSizeOptions: ['5', '10', '20'],
-        showTotal: (tot, i) => {
-            let pageSize = page.pageSize
-            let nowPage = tot
-            return `共 ${nowPage} 条记录 第${page.current}/${Math.ceil(tot / pageSize)}页`
+    },
+    created(){
+
+    },
+    mounted(){
+        let _this = this;
+        for (let i = 0; i < 100; i++) {
+            _this.dataSource.push({
+                key: i.toString(),
+                name: `James ${i}`,
+                age: 32,
+                address: `London Park no. ${i}`,
+            });
         }
-    })
+    },
+    methods: {
+        closeEdit(args){
+            console.log(args);
+        },
+        edit(record){
+            console.log(record);
+            this.modalConf.visible = true;
+        },
+        del(record){
+            console.log(record)
+        },
+        handleChange(){
 
-    const data = reactive([]);
-    for (let i = 0; i < 100; i++) {
-        data.push({
-            key: i.toString(),
-            name: `James ${i}`,
-            age: 32,
-            address: `London Park no. ${i}`,
-        });
-    }
+        },
+        changePage(pageParam){
+            this.page.current = pageParam.current;
+        },
+        handleOk(){
 
-    const test = () => {
-      http.post("/rest/admin/gbShop");
+        }
     }
-    const edit = (record) => {
-        console.log(record)
-    }
-    const del = (record) => {
-        console.log(record)
-    }
-    const handleChange = () => {
-
-    }
-    const changePage = (pageParam) => {
-        page.current = pageParam.current;
-    }
-
-    onMounted(() => {
-
-    })
-
-    onUnmounted(() => {
-      
-    })
-
-    return {
-      columns,
-      page,
-      data,
-      edit,
-      del,
-      handleChange,
-      changePage
-    }
-  }
 };
 </script>
 <style lang="scss" scoped>
